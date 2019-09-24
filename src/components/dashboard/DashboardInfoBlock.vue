0bce72d0612ded8f69fe9982c340f3f28f37a70e
exorcist <yag0da@yandex.ru>
2019-09-25 00:24:04 +0300

    init

---
+++ src/components/dashboard/DashboardInfoBlock.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="row row-equal">
+    <div class="flex xl6 xs12 mx-auto">
+      <div class="row">
+        <div class="flex xs12 sm4" v-for="(info, idx) in infoTiles" :key="idx">
+          <va-card class="mb-4" :color="info.color">
+            <p class="display-2 mb-0" style="color: white">{{ info.value }}</p>
+            <p style="color: white">{{$t('dashboard.info.' + info.text)}}</p>
+          </va-card>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="flex xs12 md6">
+          <va-card>
+            <p class="display-2 mb-1" :style="{color: this.$themes.success}">118$</p>
+            <p class="no-wrap">{{$t('dashboard.info.earned')}}</p>
+          </va-card>
+        </div>
+        <div class="flex xs12 md6">
+          <va-card>
+            <div class="row row-separated">
+              <div class="flex xs4">
+                <p class="display-2 mb-1 text--center" :style="{color: this.$themes.info}">3$</p>
+                <p class="text--center mb-1">{{$t('dashboard.info.lowCost')}}</p>
+              </div>
+              <div class="flex xs4">
+                <p class="display-2 mb-1 text--center" :style="{color: this.$themes.warning}">24$</p>
+                <p class="text--center no-wrap mb-1">{{$t('dashboard.info.mediumCost')}}</p>
+              </div>
+              <div class="flex xs4">
+                <p class="display-2 mb-1 text--center" :style="{color: this.$themes.danger}">91$</p>
+                <p class="text--center mb-1">{{$t('dashboard.info.highCost')}}</p>
+              </div>
+            </div>
+          </va-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashboardInfoBlock',
+  data () {
+    return {
+      infoTiles: [
+        {
+          color: 'secondary',
+          value: '803',
+          text: 'parsed',
+          icon: '',
+        },
+        {
+          color: 'info',
+          value: '57',
+          text: 'moderated',
+          icon: '',
+        },
+        {
+          color: 'success',
+          value: '5',
+          text: 'posted',
+          icon: '',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.row-separated {
+  .flex + .flex {
+    border-left: 1px solid #e3eaeb;
+  }
+
+  @include media-breakpoint-down(xs) {
+    p:not(.display-2) {
+      font-size: 0.875rem;
+    }
+  }
+}
+
+.dashboard {
+  .va-card__header--over {
+    @include media-breakpoint-up(md) {
+      padding-top: 0 !important;
+    }
+  }
+
+  .va-card__image {
+    @include media-breakpoint-up(md) {
+      padding-bottom: 0 !important;
+    }
+  }
+}
+</style>
---
+++ src/components/dashboard/dashboard-tabs/ProductCardTab.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="pt-2">
+    <div class="row row-equal">
+      <div class="flex xs12 md6">
+        <div
+          class="title text-dark mb-3"
+          :style="{color: $themes.primary}"
+        >{{$t('dashboard.tabs.billingAddress.personalInfo')}}</div>
+        <va-input
+          type="textarea"
+          autosize
+          :label="$t('dashboard.tabs.billingAddress.firstName')"
+          v-model="form.name"
+        />
+        <va-input :label="$t('dashboard.tabs.billingAddress.email')" v-model="form.email" />
+        <va-input :label="$t('dashboard.tabs.billingAddress.address')" v-model="form.address" />
+      </div>
+      <div class="flex xs12 md6">
+        <div
+          class="title text-dark mb-3"
+          :style="{color: $themes.primary}"
+        >{{$t('dashboard.tabs.billingAddress.companyInfo')}}</div>
+        <va-input :label="$t('dashboard.tabs.billingAddress.city')" v-model="form.city" />
+        <va-input :label="$t('dashboard.tabs.billingAddress.country')" v-model="form.country" />
+        <va-checkbox
+          :label="$t('dashboard.tabs.billingAddress.infiniteConnections')"
+          v-model="form.connection"
+        />
+      </div>
+    </div>
+    <div class="flex xs12 row row-equal">
+      <va-card
+        v-for="i of 10"
+        :key="i"
+        class="flex xs3 w-100"
+        style="height:15em;"
+        image="https://i.imgur.com/qSykGko.jpg"
+        square
+        titleOnImage
+      >
+        <template slot="header">
+          <va-button
+            :color="images.includes(i)?'success':'danger'"
+            @click="images.includes(i)?images.splice(images.findIndex(it=>it===i),1):images.push(i)"
+            class="ma-0 mb-3"
+          >{{ images.includes(i)?'✔':'✗' }}</va-button>
+        </template>
+      </va-card>
+    </div>
+    <div class="row justify--center">
+      <va-button>Add Connection</va-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'billing-address-tab',
+  data () {
+    return {
+      form: {
+        name: 'John Smith',
+        email: 'smith@gmail.com',
+        address: '93  Guild Street',
+        city: 'London',
+        country: 'United Kingdom',
+        connection: true,
+      },
+      images: [],
+    }
+  },
+}
+</script>
